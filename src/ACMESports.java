a8a88626436877bd7c92f187ffe26340f64a9430
Marina Luisa Davila Rocha <devmarinadavila@gmail.com>
2024-07-24 14:48:25 -0300

    trabalho atualizado

--- src/ACMESports.java
+++ src/ACMESports.java
@@ -1,3 +1,9 @@
 public class ACMESports {
+    public ACMESports(){
+
+    }
     
+    public void execute(){
+        System.out.println("hello");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ACMESports acmes = new ACMESports();
+        acmes.execute();
     }
 }
